79c3d874a07857f37f597857d4170924c677882b
BeardedPlatypus <mwtegelaers@gmail.com>
2020-12-01 22:11:35 +0100

    Clean up day 1 exercise

--- advent-of-code-2020/advent-of-code-2020/Day1.fs
+++ advent-of-code-2020/advent-of-code-2020/Day1.fs
@@ -6,38 +6,8 @@ module Day1 =
         |> List.ofSeq
         |> List.map int
         |> List.sort
-    
-    let rec findPairSpecificRec (goalValue: int) (lhs: int) (rhsList: int list) : (int * int) Option  =
-        match rhsList with
-        | [] -> None
-        | rhs :: _ when (rhs + lhs) = goalValue -> Some (rhs, lhs)
-        | rhs :: _ when (rhs + lhs) > goalValue -> None
-        | _   :: tail -> findPairSpecificRec goalValue lhs tail
         
-    let rec findPairRec (goalValue: int) (lhsList: int list) (rhsList: int list) : (int * int) Option =
-        if lhsList |> List.isEmpty then
-            None
-        else
-            let res = findPairSpecificRec goalValue (List.head lhsList) rhsList
-            
-            match res with
-            | Some _ -> res
-            | None   -> findPairRec goalValue (List.tail lhsList) rhsList
-        
-    let findPair (goalValue: int) (input: int list) : (int * int) Option =
-        let reversed = input |> List.rev
-        
-        findPairRec goalValue reversed input
-        
-    let public calculateProblem1 () =
-        let inputValues = inputValues ()
-        let res = findPair 2020 inputValues
-
-        match res with
-        | Some (lhs, rhs) -> lhs * rhs 
-        | None            -> -1
-        
-    let yieldProblem2 (goalValue: int) (input: int list) (nValues: int) : (int * int) Option =
+    let calculate (goalValue: int) (input: int list) (nValues: int) : (int * int) Option =
         let mutable elements = List.zip input input
         for i in 2 .. nValues do
             elements <- [ for (e1, e2) in elements do
@@ -47,17 +17,17 @@ module Day1 =
                 
         List.tryFind (fun (e1, _) -> e1 = goalValue) elements
                 
-    let public calculateProblem2 () =
+    let public calculateProblem1 () =
         let inputValues = inputValues ()
-        let res = yieldProblem2 2020 inputValues 2
+        let res = calculate 2020 inputValues 2
 
         match res with
         | Some (_, res) -> res
         | None          -> -1
         
-    let public calculateProblem3 () =
+    let public calculateProblem2 () =
         let inputValues = inputValues ()
-        let res = yieldProblem2 2020 inputValues 3
+        let res = calculate 2020 inputValues 3
 
         match res with
         | Some (_, res) -> res
--- advent-of-code-2020/advent-of-code-2020/Program.fs
+++ advent-of-code-2020/advent-of-code-2020/Program.fs
@@ -4,9 +4,9 @@ open advent_of_code_2020
 
 [<EntryPoint>]
 let main argv =
-    let result = Day1.calculateProblem2 ()
+    let result = Day1.calculateProblem1 ()
     printfn "%d" result
     
-    let result = Day1.calculateProblem3 ()
+    let result = Day1.calculateProblem2 ()
     printfn "%d" result
     0 // return an integer exit code
